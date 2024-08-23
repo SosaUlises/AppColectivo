7497f8b1ecafb4ce906f4c6ab95824df94953315
SosaUlises <ulipvp@gmail.com>
2024-08-22 22:41:31 -0300

    Gestores

---
+++ Controladora/GestorColectivo.cs
@@ -0,0 +1,76 @@
+﻿using Modelo;
+
+namespace Controladora
+{
+    public class GestorColectivo
+    {
+        private static GestorColectivo instancia;
+
+        public static GestorColectivo Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new GestorColectivo();
+                }
+                return instancia;
+            }
+        }
+        private GestorColectivo() { }
+
+        public string VenderBoleto(Pasajero unPasajero, Colectivo unColectivo, RepositorioBoletos repositorioBoletos)
+        {
+
+            if (unColectivo.Asientos > unColectivo.Boletos.Count )
+            {
+                var existePasajero = unColectivo.Boletos.FirstOrDefault(a => a.Pasajero.Dni == unPasajero.Dni);
+                if (existePasajero == null)
+                {
+                    Boleto nuevoBoleto = new Boleto();
+                    nuevoBoleto.Pasajero = unPasajero;
+                    nuevoBoleto.NroBoleto = unColectivo.Boletos.Count;
+                    nuevoBoleto.Precio = unColectivo.PrecioBoleto;
+
+                    repositorioBoletos.Agregar(nuevoBoleto); // Guardamos todos los boletos en repositorio
+                    unColectivo.agregarBoleto(nuevoBoleto); // Guardamos ese boleto especifico en su lista del colectivo
+                    return "Boleto vendido";
+                }
+                else
+                {
+                    return "Este pasajero ya cuenta con boleto!";
+                }
+            }
+            else
+            {
+                return "Colectivo lleno";
+            }
+        }
+
+        public string AgregarColectivo(Colectivo nuevoColectivo, RepositorioColectivo repositorioColectivo)
+        {
+            var existeColectivo = repositorioColectivo.Colectivos.FirstOrDefault(a => a.Patente == nuevoColectivo.Patente);
+            if(existeColectivo == null)
+            {
+                repositorioColectivo.Agregar(nuevoColectivo);
+                return "Colectivo agregado";
+            }
+            else
+            {
+                return "El colectivo ya existe!!";
+            }
+            
+        }
+        public string EliminarColectivo(Colectivo unColectivo, RepositorioColectivo repositorioColectivo)
+        {
+            repositorioColectivo.Eliminar(unColectivo);
+            return "Colectivo eliminado";
+        }
+
+        public string ModificarColectivo(Colectivo unColectivo, RepositorioColectivo repositorioColectivo)
+        {
+            repositorioColectivo.Modificar(unColectivo);
+            return "Colectivo modificado";
+        }
+    }
+}
---
+++ Controladora/GestorPasajero.cs
@@ -0,0 +1,52 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controladora
+{
+    public class GestorPasajero
+    {
+        private static GestorPasajero instancia;
+
+        public static GestorPasajero Instancia
+        {
+            get
+            {
+                if(instancia == null)
+                {
+                    instancia = new GestorPasajero();
+                }
+                return instancia;
+            }
+        }
+        private GestorPasajero() { }
+        public string AgregarPasajero(Pasajero nuevoPasajero, RepositorioPasajero repositorioPasajero)
+        {
+            var existePasajero = repositorioPasajero.Pasajeros.FirstOrDefault(a => a.Dni == nuevoPasajero.Dni);
+            if (existePasajero == null)
+            {
+                repositorioPasajero.Agregar(nuevoPasajero);
+                return "Pasajero agregado";
+            }
+            else
+            {
+                return "El pasajero ya existe!!";
+            }
+        }
+
+        public string EliminarPasajero(Pasajero unPasajero, RepositorioPasajero repositorioPasajero)
+        {
+            repositorioPasajero.Eliminar(unPasajero);
+            return "Pasajero eliminado";
+        }
+
+        public string ModificarPasajero(Pasajero unPasajero, RepositorioPasajero repositorioPasajero)
+        {
+            repositorioPasajero.Modificar(unPasajero);
+            return "Pasajero modificado";
+        }
+    }
+}
